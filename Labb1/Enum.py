177343b3ff2312d2fd09df304e60b6de23f0e86f
clahul-9 <clahul-9@student.ltu.se>
2021-03-27 16:54:53 +0100

    Enum + foodState

---
+++ Labb1/Enum.py
@@ -0,0 +1,7 @@
+class PlaseEnum:
+    HOME        = 0
+    BAR         = 1
+    RESTURANT   = 2
+    STORE       = 3
+    FACTORY     = 4
+    OFFICE      = 5
--- Labb1/State.py
+++ Labb1/State.py
@@ -1,42 +1,56 @@
+from Labb1 import Enum
 class State:
     def Enter(self, agent):
         pass
     def Execute(self, agent):
         pass
-    def Exit(self):
-        pass
-
-    def __del__(self):
+    def Exit(self, agent):
         pass
 
 class State1(State):
     def Enter(self, agent):
         print("entering State 1")
+
     def Execute(self, agent):
         print("executeing State 1")
-    def Exit(self):
-        print("Exiting State 1")
 
+    def Exit(self, agent):
+        print("Exiting State 1")
 
 class State2(State):
     def Enter(self, agent):
         print("entering State 2")
+
     def Execute(self, agent):
         print("executeing State 2")
+
     def Exit(self, agent):
         print("Exiting State 2")
 
 class gotoBarAndDrink(State):
     def Enter(self, agent):
-        if self.place != "bar":
-            self.place = "bar"
-            print( "entering bar")
+        if agent.place != Enum.PlaseEnum.BAR:
+            agent.place = Enum.PlaseEnum.BAR
+            print(agent.get_ID + ": entering bar")
+
     def Execute(self, agent):
         agent.thirst += 1
-        print("Ahh, god drinck")
+        print(agent.get_ID + ": Ahh, good drink, really clenhes your thirst")
+        agent.WhatToDo()
+
+    def Exit(self, agent):
+        print(agent.get_ID + ": time to leave the bar")
 
 class gotoTheResturantAndEat(State):
-    pass
+    def Enter(self, agent):
+        if agent.place != Enum.PlaseEnum.RESTURANT:
+            agent.place = Enum.PlaseEnum.RESTURANT
+            print(agent.get_ID + ": Entering the Resturant")
+    def Execute(self, agent):
+        agent.hunger += 1
+        print(agent.get_ID + ": This food is tasty")
+    def Exit(self, agent):
+        print(agent.get_ID + ": I am full, time to leave the Resturant.")
 
 class goHomeandSleepUntilRested(State):
     pass
